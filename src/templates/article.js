ffcdee67b86da7770c53b73bec4f7ff2da6f628a
Phillip Mello <phill@metricasboss.com.br>
2019-10-24 18:53:43 -0300

    estilizando base do template de posts

--- src/templates/article.js
+++ src/templates/article.js
@@ -5,10 +5,69 @@ import Layout from '../components/layout'
 
 const ArticleTemplate = ({ data }) => (
   <Layout>
-    <h1>{data.strapiArticle.title}</h1>
-    <p>by <Link to={`/authors/User_${data.strapiArticle.author.id}`}>{data.strapiArticle.author.username}</Link></p>
-    <Img fluid={data.strapiArticle.feature.childImageSharp.fluid}/>
-    <p>{data.strapiArticle.content}</p>
+        <section class="section">
+            <div class="container">
+                <div class="tile is-ancestor">
+                    <div className="tile is-parent is-8">
+                        <div className="tile is-child box">
+                            <h1 className="title">{data.strapiArticle.title}</h1>
+                            <p>{data.strapiArticle.content}</p>
+                        </div>
+                    </div>
+                    <div className="tile is-parent is-4 is-vertical">
+                        <div className="tile is-child box">
+                            <div className="media">
+                                <div class="media-left">
+                                    <figure class="image is-64x64">
+                                        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <div class="content">
+                                        <h5 className="title is-6">
+                                            <Link className="has-text-grey-dark"
+                                              to={`/authors/User_${data.strapiArticle.author.id}`}>
+                                                {data.strapiArticle.author.username}
+                                            </Link>
+                                        </h5>
+                                        <Link className="has-text-grey-dark is-size-7"
+                                              to={`#`}>Ver mais posts</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tile is-child box">
+                            <p></p>
+                        </div>
+                        <div className="tile is-child box">
+                            <h4 className="title is-4">Posts por categoria</h4>
+                            <ul>
+                                <li><Link className="has-text-grey-dark" 
+                                          to={`#`}>
+                                            Web Analytics
+                                    </Link>
+                                </li>
+                                <li><Link className="has-text-grey-dark" 
+                                          to={`#`}>
+                                            Google Analytics
+                                    </Link>
+                                </li>
+                                <li><Link className="has-text-grey-dark"
+                                          to={`#`}>
+                                            Google Tag Manager
+                                    </Link>
+                                </li>
+                                <li><Link className="has-text-grey-dark"
+                                          to={`#`}>
+                                            Podcast de Web Analytics
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </section> 
   </Layout>
 )
 
