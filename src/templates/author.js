ffccd348b79bd85bbfe2c785de53c529d5877e45
Phillip Mello <phill@metricasboss.com.br>
2019-10-31 16:52:22 -0300

    criando pagina do usuario

--- src/templates/author.js
+++ src/templates/author.js
@@ -1,36 +1,60 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
-import SEO from "../components/seo"
+import SEO from '../components/seo'
+import Grid from '../components/grid'
 
 const UserTemplate = ({ data }) => (
     <Layout>
-      <SEO title={data.strapiUser.username} />  
-      <h1>{data.strapiUser.username}</h1>
-      <ul>
-        {data.strapiUser.articles.map(article => (
-          <li key={article.id}>
-            <h2>
-              <Link to={`/Article_${article.id}`}>{article.title}</Link>
-            </h2>
-            <p>{article.content}</p>
-          </li>
-        ))}
-      </ul>
+      <SEO title={data.strapiUser.username} />
+      <section className="section">
+        <div className="container">
+          <h2 className="title has-text-dark has-text-weight-bold">Posts criados por: {data.strapiUser.username}</h2>
+          <Grid posts={data.allStrapiArticle.edges} />
+        </div>
+      </section>
+
     </Layout>
   )
+
   
 export default UserTemplate
 
 export const query = graphql`
-  query UserTemplate($id: String!) {
-    strapiUser(id: { eq: $id }) {
+  query UserTemplate($username: String) {
+    strapiUser(username: { eq: $username }) {
       id
       username
       articles {
         id
         title
         content
+      }
+    }
+    allStrapiArticle(filter: {author: {username: {eq: $username}}}) {
+      edges {
+        node {
+           id,
+           title,
+           content,
+           feature {
+             id
+             childImageSharp {
+               fluid(maxWidth: 320) {
+                 ...GatsbyImageSharpFluid
+               }
+             }
+           },
+           author {
+             id,
+             username
+           },
+           categories {
+             id,
+             name,
+             slug
+           }
+        }
       }
     }
   }
