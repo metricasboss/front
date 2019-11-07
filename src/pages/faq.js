f90fd523f81b742b1690c72ac947582175fc67e4
Lucian Fialho <lucianfialhobp@hotmail.com>
2019-11-07 11:15:31 -0300

    -- subindo estrutura inicial do faq

---
+++ src/pages/faq.js
@@ -0,0 +1,24 @@
+import { graphql } from "gatsby"
+
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Grid from "../components/grid"
+
+const FaqPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+      <section className="section">
+        <div className="container">
+         
+        </div>
+      </section>
+  </Layout>
+)
+
+export default FaqPage
+
+export const pageQuery = graphql`
+
+`
+
