e52b42fac906f5407814a9436aaf78bdef986cca
Raul <rplinks@gmail.com>
2019-11-08 00:09:46 -0300

    Página FAQ estrutura

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,4 +1,4 @@
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import React from "react"
 import Layout from "../components/layout"
@@ -8,10 +8,82 @@ import Grid from "../components/grid"
 const FaqPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-      <section className="section">
-        <div className="container">
-         
+      <section className="section container is-fluid">
+
+        <div className="columns is-mobile">
+          <div className="column is-full">
+            <h2 className="has-text-centered is-size-3">Como podemos ajudar?</h2>
+          </div>
         </div>
+
+        <div className="columns is-desktop">
+          <div className="column is-three-fifths is-offset-one-fifth">
+            <input class="input is-medium is-rounded" type="text" placeholder="Digite palavras-chaves para encontrar sua dúvida"></input>
+          </div>
+        </div>
+        
+      </section>
+
+      <section className="section container is-widescreen">
+        <div className="columns is-desktop">
+          <div className="column">
+            <h1 className="is-size-4">Perguntas Frequentes</h1>
+          </div>
+        </div>
+
+        <div className="columns is-desktop">
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+        </div>
+
+        <div className="columns is-desktop">
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+          <div className="column is-one-third">
+            <h3 className="is-size-5">Google Analytics</h3>
+            <ul>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+              <li><Link>O que é Google Analytics</Link></li>
+            </ul>   
+          </div>
+        </div>  
       </section>
   </Layout>
 )
